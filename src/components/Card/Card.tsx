a8fc3d2263c7123d9f736a73f75d5c35caf3146c
Evgenii Kolesnikov <jekow@yandex.ru>
2023-03-20 06:46:29 +0300

    feat: add Card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,23 @@
+import { cardType } from 'data/CardsData';
+import React, { Component } from 'react';
+import './Card.scss';
+
+interface State {
+  name: string;
+}
+
+export default class Card extends Component<cardType, State> {
+  constructor(card: cardType) {
+    super(card);
+  }
+  render() {
+    return (
+      <div className="card">
+        <img className="card__image" src={this.props.img} alt="image" />
+        <div className="card__price">{this.props.price}</div>
+        <div className="card__brand">{this.props.brand}</div>
+        <div className="card__desc">{this.props.desc}</div>
+      </div>
+    );
+  }
+}
