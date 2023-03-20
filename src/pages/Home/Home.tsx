c68f49c8fb6f2e6dc737dfd6f4bbf42f66431f7f
Evgenii Kolesnikov <jekow@yandex.ru>
2023-03-20 05:00:48 +0300

    feat: add SearchBar component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,16 @@
 import Header from 'components/Header/Header';
+import SearchBar from 'components/SearchBar/SearchBar';
 import React, { Component } from 'react';
 import './Home.scss';
 
 export default class Home extends Component {
   render() {
+    console.log('Home');
+
     return (
       <>
         <Header name="Home" />
+        <SearchBar />
       </>
     );
   }
