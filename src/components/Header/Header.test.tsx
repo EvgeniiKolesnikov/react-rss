071d6dce26bce28952ef9eafde6f836cf907b95e
Evgenii Kolesnikov <jekow@yandex.ru>
2023-03-20 07:56:03 +0300

    feat: add Header tests

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Header from './Header';
+
+test('renders Header AboutUs', () => {
+  render(<Header name="AboutUs" />);
+  const linkElement = screen.getByText(/AboutUs/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Header Home', () => {
+  render(<Header name="Home" />);
+  const linkElement = screen.getByText(/Home/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Header 404', () => {
+  render(<Header name="404" />);
+  const linkElement = screen.getByText(/404/i);
+  expect(linkElement).toBeInTheDocument();
+});
