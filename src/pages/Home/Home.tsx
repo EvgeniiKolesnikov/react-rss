2286a45c9dc913528ee8a7c579608cc8fbabe2a6
Evgenii Kolesnikov <jekow@yandex.ru>
2023-03-20 03:04:54 +0300

    feat: add Home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import Header from 'components/Header/Header';
+import React, { Component } from 'react';
+import './Home.scss';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <>
+        <Header name="Home" />
+      </>
+    );
+  }
+}
