63a14e914826591c2361f226c1584d6350463fd0
Evgenii Kolesnikov <jekow@yandex.ru>
2023-03-20 03:06:00 +0300

    feat: add AboutUs page

---
+++ src/pages/AboutUs/AboutUs.tsx
@@ -0,0 +1,9 @@
+import Header from 'components/Header/Header';
+import React, { Component } from 'react';
+import './AboutUs.scss';
+
+export default class AboutUs extends Component {
+  render() {
+    return <Header name="About Us" />;
+  }
+}
