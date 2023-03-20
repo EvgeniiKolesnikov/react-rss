7ee08e92b093a4f66f7e5634e45e29456ba2bbe7
Evgenii Kolesnikov <jekow@yandex.ru>
2023-03-20 12:08:51 +0300

    feat: add SearchBar test for Uncovered Line

--- src/components/SearchBar/SearchBar.test.tsx
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -1,17 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import SearchBar from './SearchBar';
 import '@testing-library/jest-dom';
 
 describe('SearchBar component', () => {
   it('SearchBar placeholder', () => {
     render(<SearchBar />);
-    const linkElement = screen.getByPlaceholderText(/Search.../i);
-    expect(linkElement).toBeInTheDocument();
+    const SearchBarElement = screen.getByPlaceholderText(/Search.../i);
+    expect(SearchBarElement).toBeInTheDocument();
   });
 
   it('SearchBar title', () => {
     render(<SearchBar />);
-    const linkElement = screen.getByTitle('search-bar');
-    expect(linkElement).toBeInTheDocument();
+    const SearchBarElement = screen.getByTitle('search-bar');
+    expect(SearchBarElement).toBeInTheDocument();
+  });
+
+  it('SearchBar input', () => {
+    render(<SearchBar />);
+    const SearchBarElement = screen.getByTitle<HTMLInputElement>('search-bar');
+    fireEvent.change(SearchBarElement, { target: { value: 'Lebron James' } });
+    expect(SearchBarElement.value).toEqual('Lebron James');
   });
 });
