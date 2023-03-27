6e909444584d2764ab06b18fd7a85741a7aa1c5b
Evgenii Kolesnikov <jekow@yandex.ru>
2023-03-27 06:45:02 +0300

    refactor: optimize Cards component

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -12,14 +12,7 @@ export default class Cards extends Component<Record<string, never>, State> {
     return (
       <div className="cards">
         {CardsData.map((card: cardType) => (
-          <Card
-            key={card.id}
-            id={card.id}
-            img={card.img}
-            price={card.price}
-            brand={card.brand}
-            desc={card.desc}
-          />
+          <Card key={card.id} {...card} />
         ))}
       </div>
     );
