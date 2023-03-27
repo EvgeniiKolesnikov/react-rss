76de3be941ff40dc5d14ac79ac1adceba3dbfc9d
Evgenii Kolesnikov <jekow@yandex.ru>
2023-03-27 13:00:36 +0300

    feat: udpate FormCard test

--- src/components/FormCard/FormCard.test.tsx
+++ src/components/FormCard/FormCard.test.tsx
@@ -1,59 +1,90 @@
 import { render, screen } from '@testing-library/react';
 import FormCard from './FormCard';
 import '@testing-library/jest-dom';
+import sampleCard from '../../assets/card/card1.jpg';
 
-const Card = {
-  Name: 'Tom',
-  Date: '1111-11-11',
-  Pet: 'Dog',
-  Assent: true,
-  Gender: 'Male',
-  img: null,
+const card = {
+  name: 'Tom',
+  date: '1111-11-11',
+  pet: 'Dog',
+  assent: true,
+  gender: 'Male',
+  img: sampleCard,
 };
 
 describe('FormCard component', () => {
   it('Render Pet', () => {
     render(
       <FormCard
-        name={Card.Name}
-        date={Card.Date}
-        pet={Card.Pet}
-        assent={Card.Assent}
-        gender={Card.Gender}
-        img={Card.img}
+        name={card.name}
+        date={card.date}
+        pet={card.pet}
+        assent={card.assent}
+        gender={card.gender}
+        img={card.img}
       />
     );
-    const text = screen.getByText(`Pet: ${Card.Pet}`);
+    const text = screen.getByText(`Pet: ${card.pet}`);
     expect(text).toBeInTheDocument();
   });
 
   it('Render Gender', () => {
     render(
       <FormCard
-        name={Card.Name}
-        date={Card.Date}
-        pet={Card.Pet}
-        assent={Card.Assent}
-        gender={Card.Gender}
-        img={Card.img}
+        name={card.name}
+        date={card.date}
+        pet={card.pet}
+        assent={card.assent}
+        gender={card.gender}
+        img={card.img}
       />
     );
-    const text = screen.getByText(`Gender: ${Card.Gender}`);
+    const text = screen.getByText(`Gender: ${card.gender}`);
     expect(text).toBeInTheDocument();
   });
 
-  it('Render Assent', () => {
+  it('Render Assent Yes', () => {
     render(
       <FormCard
-        name={Card.Name}
-        date={Card.Date}
-        pet={Card.Pet}
-        assent={Card.Assent}
-        gender={Card.Gender}
-        img={Card.img}
+        name={card.name}
+        date={card.date}
+        pet={card.pet}
+        assent={card.assent}
+        gender={card.gender}
+        img={card.img}
       />
     );
-    const text = screen.getByText(`Assent: ${Card.Assent ? 'Yes' : 'No'}`);
+    const text = screen.getByText(/Assent: Yes/i);
     expect(text).toBeInTheDocument();
+  });
+
+  it('Render Assent No', () => {
+    render(
+      <FormCard
+        name={card.name}
+        date={card.date}
+        pet={card.pet}
+        assent={false}
+        gender={card.gender}
+        img={card.img}
+      />
+    );
+    const text = screen.getByText(/Assent: No/i);
+    expect(text).toBeInTheDocument();
+  });
+
+  it('Render Img', () => {
+    render(
+      <FormCard
+        name={card.name}
+        date={card.date}
+        pet={card.pet}
+        assent={card.assent}
+        gender={card.gender}
+        img={card.img}
+      />
+    );
+    const image = screen.getByAltText(`card-image`);
+    expect(image).toHaveAttribute('src', card.img);
   });
 });
