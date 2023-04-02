f18bfb2ab621eedca04e95dbdc7653e1c8ece01f
Evgenii Kolesnikov <jekow@yandex.ru>
2023-04-02 23:45:03 +0300

    refactor: convert FormSubmitMessage component

--- src/components/FormSubmitMessage/FormSubmitMessage.tsx
+++ src/components/FormSubmitMessage/FormSubmitMessage.tsx
@@ -1,8 +1,5 @@
-import React, { Component } from 'react';
 import './FormSubmitMessage.scss';
 
-export default class FormSubmitMessage extends Component {
-  render() {
-    return <div className="form__submit-message">New Card data has been saved</div>;
-  }
+export default function FormSubmitMessage() {
+  return <div className="form__submit-message">New Card data has been saved</div>;
 }
