61db33360f1a11a7c589de62a48baadab9514b74
Evgenii Kolesnikov <jekow@yandex.ru>
2023-04-03 01:32:19 +0300

    refactor: convert Form page

--- src/pages/Form/Form.tsx
+++ src/pages/Form/Form.tsx
@@ -2,7 +2,7 @@ import FormCard, { FormCardType } from 'components/FormCard/FormCard';
 import FormSubmitMessage from 'components/FormSubmitMessage/FormSubmitMessage';
 import Header from 'components/Header/Header';
 import LabelError from 'components/LabelError/LabelError';
-import React, { Component } from 'react';
+import React, { useRef, useState } from 'react';
 import './Form.scss';
 
 type ErrorsType = {
@@ -14,44 +14,30 @@ type ErrorsType = {
   img: string | null;
 };
 
-interface Props {
-  value?: string;
-}
+type Input = React.RefObject<HTMLInputElement>;
+type Select = React.RefObject<HTMLSelectElement>;
 
-interface State {
-  isShowMessage: boolean;
-  errors: ErrorsType;
-  cards: FormCardType[];
-}
+export default function Form() {
+  const nameInput: Input = useRef(null);
+  const dateInput: Input = useRef(null);
+  const selectPetInput: Select = useRef(null);
+  const checkboxAssentInput: Input = useRef(null);
+  const radioInputMale: Input = useRef(null);
+  const radioInputFemale: Input = useRef(null);
+  const fileInput: Input = useRef(null);
 
-export default class Form extends Component<Props, State> {
-  private nameInput: React.RefObject<HTMLInputElement>;
-  private dateInput: React.RefObject<HTMLInputElement>;
-  private selectPetInput: React.RefObject<HTMLSelectElement>;
-  private checkboxAssentInput: React.RefObject<HTMLInputElement>;
-  private radioInputMale: React.RefObject<HTMLInputElement>;
-  private radioInputFemale: React.RefObject<HTMLInputElement>;
-  private fileInput: React.RefObject<HTMLInputElement>;
+  const [isShowMessage, setIsShowMessage] = useState<boolean>(false);
+  const [formErrors, setFormErrors] = useState<ErrorsType>({
+    name: '',
+    date: '',
+    pet: '',
+    assent: '',
+    gender: '',
+    img: null,
+  });
+  const [cards, setCards] = useState<FormCardType[]>([]);
 
-  constructor(props: Props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.nameInput = React.createRef();
-    this.dateInput = React.createRef();
-    this.selectPetInput = React.createRef();
-    this.checkboxAssentInput = React.createRef();
-    this.radioInputMale = React.createRef();
-    this.radioInputFemale = React.createRef();
-    this.fileInput = React.createRef();
-
-    this.state = {
-      isShowMessage: false,
-      errors: { name: '', date: '', pet: '', assent: '', gender: '', img: null },
-      cards: [],
-    };
-  }
-
-  validate(newCard: FormCardType) {
+  function validate(newCard: FormCardType) {
     const errors: ErrorsType = {
       name: newCard.name.search(/^([A-ZА-Я][a-zа-я]{2,15})/)
         ? 'The Name should consist of 3-11 letters and start with a Capital letter'
@@ -66,34 +52,29 @@ export default class Form extends Component<Props, State> {
     return errors;
   }
 
-  handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const formElem = e.target as HTMLFormElement;
 
     const newCard: FormCardType = {
-      name: this.nameInput.current!.value,
-      date: this.dateInput.current!.value,
-      pet: this.selectPetInput.current!.value,
-      assent: this.checkboxAssentInput.current!.checked,
-      gender: this.radioInputMale.current!.checked
+      name: nameInput.current!.value,
+      date: dateInput.current!.value,
+      pet: selectPetInput.current!.value,
+      assent: checkboxAssentInput.current!.checked,
+      gender: radioInputMale.current!.checked
         ? 'Male'
-        : this.radioInputFemale.current!.checked
+        : radioInputFemale.current!.checked
         ? 'Female'
         : '',
       img:
-        (this.fileInput.current?.files &&
-          this.fileInput.current?.files.length > 0 &&
-          URL.createObjectURL(this.fileInput.current!.files![0])) ||
+        (fileInput.current?.files &&
+          fileInput.current?.files.length > 0 &&
+          URL.createObjectURL(fileInput.current!.files![0])) ||
         null,
     };
 
-    const errors = this.validate(newCard);
-
-    this.setState((state) => ({
-      isShowMessage: state.isShowMessage,
-      errors: errors,
-      cards: [...state.cards],
-    }));
+    const errors = validate(newCard);
+    setFormErrors(errors);
 
     const hasAnyError = Object.values(errors).some((v) => v || v !== '');
 
@@ -101,120 +82,109 @@ export default class Form extends Component<Props, State> {
       return;
     }
 
-    this.setState((state) => ({
-      isShowMessage: state.isShowMessage,
-      errors: { name: '', date: '', pet: '', assent: '', gender: '', img: null },
-      cards: [...state.cards, newCard],
-    }));
+    setFormErrors({ name: '', date: '', pet: '', assent: '', gender: '', img: null });
+    setCards((prevCards) => [...prevCards, newCard]);
 
     formElem.reset();
-    this.showSubmitMessage();
+    showSubmitMessage();
   }
 
-  showSubmitMessage(): void {
-    this.setState((prevState) => ({
-      isShowMessage: true,
-      cards: prevState.cards,
-    }));
+  function showSubmitMessage(): void {
+    setIsShowMessage(true);
 
     const timer: NodeJS.Timeout = setTimeout(() => {
-      this.setState((prevState) => ({
-        isShowMessage: false,
-        cards: prevState.cards,
-      }));
+      setIsShowMessage(false);
       clearTimeout(timer);
     }, 3000);
   }
 
-  render() {
-    return (
-      <>
-        <Header name="Form" />
-        <form className="form" onSubmit={this.handleSubmit.bind(this)}>
-          <div className="form__item">
-            <LabelError value={this.state.errors.name} />
-            <label className="form__label">Name:</label>
-            <input
-              className="form__input"
-              type="text"
-              ref={this.nameInput}
-              placeholder="Enter your name"
-            />
-          </div>
-
-          <div className="form__item">
-            <LabelError value={this.state.errors.date} />
-            <label className="form__label">Date:</label>
-            <input
-              className="form__input"
-              type="date"
-              max="9999-12-31"
-              ref={this.dateInput}
-              placeholder="Enter date"
-            />
-          </div>
-
-          <div className="form__item">
-            <LabelError value={this.state.errors.pet} />
-            <label className="form__label">Your pet:</label>
-            <select className="form__select" ref={this.selectPetInput}>
-              <option value="">Set your pet</option>
-              <option value="No pet">No pet</option>
-              <option value="Dog">Dog</option>
-              <option value="Cat">Cat</option>
-              <option value="Other">Other</option>
-            </select>
-          </div>
-
-          <div className="form__item">
-            <LabelError value={this.state.errors.assent} />
-            <label className="form__label">Cross my heart and hope to die. It is true</label>
-            <input className="form__input" type="checkbox" ref={this.checkboxAssentInput} />
-          </div>
-
-          <div className="form__item">
-            <LabelError value={this.state.errors.gender} />
-            <label className="form__label">Gender:</label>
-            <input
-              className="form__radio"
-              type="radio"
-              name="gender"
-              value="male"
-              ref={this.radioInputMale}
-            />
-            Male
-            <input
-              className="form__radio"
-              type="radio"
-              name="gender"
-              value="female"
-              ref={this.radioInputFemale}
-            />
-            Female
-          </div>
-
-          <div className="form__item">
-            <LabelError value={this.state.errors.img} />
-            <label className="form__label"></label>
-            <input
-              className="form__input"
-              type="file"
-              ref={this.fileInput}
-              placeholder="Add file"
-              accept="image/*"
-            />
-          </div>
-
-          <input className="form__submit" type="submit" value="Submit" />
-        </form>
-
-        {this.state.isShowMessage && <FormSubmitMessage />}
-        <div className="form__cards">
-          {this.state.cards.map((formCard: FormCardType, i: number) => (
-            <FormCard key={i} {...formCard} />
-          ))}
+  return (
+    <>
+      <Header name="Form" />
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form__item">
+          <LabelError value={formErrors.name} />
+          <label className="form__label">Name:</label>
+          <input
+            className="form__input"
+            type="text"
+            ref={nameInput}
+            placeholder="Enter your name"
+          />
         </div>
-      </>
-    );
-  }
+
+        <div className="form__item">
+          <LabelError value={formErrors.date} />
+          <label className="form__label">Date:</label>
+          <input
+            className="form__input"
+            type="date"
+            max="9999-12-31"
+            ref={dateInput}
+            placeholder="Enter date"
+          />
+        </div>
+
+        <div className="form__item">
+          <LabelError value={formErrors.pet} />
+          <label className="form__label">Your pet:</label>
+          <select className="form__select" ref={selectPetInput}>
+            <option value="">Set your pet</option>
+            <option value="No pet">No pet</option>
+            <option value="Dog">Dog</option>
+            <option value="Cat">Cat</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
+
+        <div className="form__item">
+          <LabelError value={formErrors.assent} />
+          <label className="form__label">Cross my heart and hope to die. It is true</label>
+          <input className="form__input" type="checkbox" ref={checkboxAssentInput} />
+        </div>
+
+        <div className="form__item">
+          <LabelError value={formErrors.gender} />
+          <label className="form__label">Gender:</label>
+          <input
+            className="form__radio"
+            type="radio"
+            name="gender"
+            value="male"
+            ref={radioInputMale}
+          />
+          Male
+          <input
+            className="form__radio"
+            type="radio"
+            name="gender"
+            value="female"
+            ref={radioInputFemale}
+          />
+          Female
+        </div>
+
+        <div className="form__item">
+          <LabelError value={formErrors.img} />
+          <label className="form__label"></label>
+          <input
+            className="form__input"
+            type="file"
+            ref={fileInput}
+            placeholder="Add file"
+            accept="image/*"
+          />
+        </div>
+
+        <input className="form__submit" type="submit" value="Submit" />
+      </form>
+
+      {isShowMessage && <FormSubmitMessage />}
+      <div className="form__cards">
+        {cards.map((formCard: FormCardType, i: number) => (
+          <FormCard key={i} {...formCard} />
+        ))}
+      </div>
+    </>
+  );
 }
