c1ea4cbda02ba09a0d3571370ded646ed3e40638
Evgenii Kolesnikov <jekow@yandex.ru>
2023-04-02 23:31:27 +0300

    refactor: convert Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,9 @@
-import React, { Component } from 'react';
 import './Header.scss';
 
 interface Props {
   name: string;
 }
 
-interface State {
-  name: string;
-}
-
-export default class Header extends Component<Props, State> {
-  render() {
-    return <div className="header">{this.props.name}</div>;
-  }
+export default function Header({ name }: Props) {
+  return <div className="header">{name}</div>;
 }
