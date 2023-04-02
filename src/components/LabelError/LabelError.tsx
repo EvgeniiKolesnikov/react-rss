1873c590aa7230dc6839427d155e8dff98975124
Evgenii Kolesnikov <jekow@yandex.ru>
2023-04-02 23:39:07 +0300

    refactor: convert LabelError component

--- src/components/LabelError/LabelError.tsx
+++ src/components/LabelError/LabelError.tsx
@@ -1,12 +1,9 @@
-import React, { Component } from 'react';
 import './LabelError.scss';
 
 interface Props {
   value: string | null;
 }
 
-export default class LabelError extends Component<Props> {
-  render() {
-    return this.props.value && <label className="form__label-error">{this.props.value}</label>;
-  }
+export default function LabelError({ value }: Props) {
+  return value ? <label className="form__label-error">{value}</label> : <></>;
 }
