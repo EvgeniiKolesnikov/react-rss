bdd6c8729ce841f1a4bff027c55080c695d86900
Evgenii Kolesnikov <jekow@yandex.ru>
2023-04-03 06:59:17 +0300

    feat: update FormCards test

--- src/components/FormCards/FormCards.test.tsx
+++ src/components/FormCards/FormCards.test.tsx
@@ -1,9 +1,19 @@
 import { render } from '@testing-library/react';
 import FormCards from './FormCards';
 import '@testing-library/jest-dom';
+import { FormCardType } from 'components/FormCard/FormCard';
+
+const cardExample: FormCardType = {
+  name: 'Jo',
+  date: '01-11-2002',
+  pet: 'Cat',
+  assent: true,
+  gender: 'male',
+  img: null,
+};
 
 test('FormCards rendered', () => {
-  const { container } = render(<FormCards formCards={[]} />);
-  const cards = container.querySelector('.form__cards');
-  expect(cards).toBeInTheDocument();
+  const { container } = render(<FormCards formCards={[cardExample]} />);
+  const formCardsElem = container.querySelector('.form__cards');
+  expect(formCardsElem).toBeInTheDocument();
 });
