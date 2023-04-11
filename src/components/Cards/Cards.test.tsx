097a0e8fe146aacb95196ff3d345557fceabe262
Evgenii Kolesnikov <jekow@yandex.ru>
2023-04-11 06:52:08 +0300

    feat: update Cards test

--- src/components/Cards/Cards.test.tsx
+++ src/components/Cards/Cards.test.tsx
@@ -1,9 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import Cards from './Cards';
 import '@testing-library/jest-dom';
+import { sampleCardsArray } from 'data/samples';
 
 test('Cards rendered', () => {
-  render(<Cards cards={[]} />);
-  const cards = screen.queryByText('cards');
-  expect(cards).not.toBeInTheDocument();
+  render(<Cards cards={sampleCardsArray} />);
+  const card = screen.getAllByText('Investigator Rick');
+  screen.debug();
+  expect(card[0]).toBeInTheDocument();
 });
