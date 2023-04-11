289f931e117922be9e770240c7430c94925e9c3f
Evgenii Kolesnikov <jekow@yandex.ru>
2023-04-11 05:44:54 +0300

    feat: add Backdrop component

---
+++ src/components/Backdrop/Backdrop.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import Backdrop from './Backdrop';
+import '@testing-library/jest-dom';
+
+describe('Backdrop component', () => {
+  it('Render Backdrop', () => {
+    render(<Backdrop closeDetails={() => {}} />);
+  });
+});
