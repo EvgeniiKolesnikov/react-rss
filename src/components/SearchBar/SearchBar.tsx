5f4289f4a04b8fbf94ebdfaa134854aeef0f49e2
Evgenii Kolesnikov <jekow@yandex.ru>
2023-04-19 08:36:24 +0300

    feat: modify SearchBar component

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,66 +1,32 @@
-import { useEffect, useState, useRef } from 'react';
-import './SearchBar.scss';
 import Cards from 'components/Cards/Cards';
 import Loader, { LoadStatus } from 'components/Loader/Loader';
-import { CardType, Data } from 'types/api';
-
-const base = 'https://rickandmortyapi.com/api';
-const character = '/character';
-const LS_VALUE = 'rss-input-value';
-const LOADING_DELAY = 700;
+import { CardType } from 'types/api';
+import { setSearchText } from 'redux/searchTextSlice';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { useEffect, useState } from 'react';
+import { useGetCharacterQuery } from 'redux/rickandmortyApi';
+import './SearchBar.scss';
 
 export default function SearchBar() {
-  const [value, setValue] = useState('');
-  const refValue = useRef(value);
+  const searchText = useAppSelector((state) => state.searchText.searchText);
+  const dispatch = useAppDispatch();
 
+  const [value, setValue] = useState(searchText);
   const [cards, setCards] = useState<CardType[]>([]);
-  const [statusText, setStatusText] = useState<LoadStatus>(LoadStatus.loading);
+
+  const { data, error, isFetching } = useGetCharacterQuery(searchText);
 
   useEffect(() => {
-    const lastValue = localStorage.getItem(LS_VALUE) || '';
-    setValue(() => lastValue);
-    refValue.current = lastValue;
-    fetchData(refValue.current);
-  }, []);
-
-  const errorData = () => {
-    setCards(() => []);
-    setStatusText(LoadStatus.failed);
-  };
-
-  const fetchData = async (value: string) => {
-    setCards(() => []);
-    setStatusText(LoadStatus.loading);
-
-    setTimeout(async () => {
-      try {
-        const res = await fetch(`${base}${character}?name=${value}`);
-        if (res.status === 200) {
-          const data: Data = await res.json();
-          setCards(data.results);
-          setStatusText(LoadStatus.loaded);
-        } else {
-          errorData();
-        }
-      } catch (error) {
-        errorData();
-      }
-    }, LOADING_DELAY);
-  };
-
-  const saveInput = () => {
-    localStorage.setItem(LS_VALUE, refValue.current);
-  };
+    data ? setCards(() => data.results) : setCards(() => []);
+  }, [data]);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-    refValue.current = e.target.value;
+    setValue(() => e.target.value);
   };
 
   const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
-      saveInput();
-      fetchData(refValue.current);
+      dispatch(setSearchText(value));
     }
   };
 
@@ -71,12 +37,13 @@ export default function SearchBar() {
         onChange={onChange}
         onKeyDown={onKeyDown}
         type="text"
-        value={value || ''}
+        value={value}
         placeholder="Search..."
         title="search-bar"
       />
-      <Loader status={statusText} />
-      <Cards cards={cards} />
+      {isFetching && <Loader status={LoadStatus.loading} />}
+      {!isFetching && error && <Loader status={LoadStatus.failed} />}
+      {!isFetching && !error && data?.results && <Cards cards={cards} />}
     </>
   );
 }
